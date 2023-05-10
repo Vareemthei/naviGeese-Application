87972449193aee539968eac236530ddb5f46bf83
Vareemthei <dmakmakmg@gmail.com>
2023-05-09 21:43:55 -0300

    trying to fix metro config..

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { homeStyles } from '../styles';
 
 const HomeScreen = ({ navigation }: any) => {
@@ -9,7 +9,6 @@ const HomeScreen = ({ navigation }: any) => {
       <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
         <Text>Go to Settings</Text>
       </TouchableOpacity>
-      <Image source={require('../assets/images/floor-plan.svg')} />
     </View>
   );
 };
